35b1cd6d049b3d89bcfe795d4fc87d6dee798e24
Shivangkpatel <Shivang02052005@gmail.com>
2023-09-01 23:01:50 +0530

    file folders update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 //app.use(express.static("views/CSS"));
+app.use(express.static("views/templates"));
 
 //global variables
 let verificationNum = 0;
@@ -22,27 +23,27 @@ let flag = 0;
 
 //Routes for rendaring the file
 app.get('/', function(req, res) {
-    res.render("home");
+    res.render("templates/home");
 });
 
 app.get('/upcomingevent', function(req, res) {
-    res.render("upcoming", { log: logedInUserData[0] });
+    res.render("templates/upcoming", { log: logedInUserData[0] });
 });
 
 app.get('/addevent', function(req, res) {
-    res.render("addevent", { log: logedInUserData[0] });
+    res.render("templates/addevent", { log: logedInUserData[0] });
 });
 
 app.get('/collab', function(req, res) {
-    res.render("collab", { log: logedInUserData[0] });
+    res.render("templates/collab", { log: logedInUserData[0] });
 });
 
 app.get('/home', function(req, res) {
-    res.render("home");
+    res.render("templates/home");
 });
 
 app.get('/Login', function(req, res) {
-    res.render("Login", { flag: 0 });
+    res.render("templates/Login", { flag: 0 });
 });
 
 app.post('/Login', async function(req, res) {
@@ -50,16 +51,16 @@ app.post('/Login', async function(req, res) {
     logedInUserData = await DB.isLogin(LoginData);
     if (logedInUserData.length != 0) {
         if (logedInUserData[0].password == LoginData.passwordLogin)
-            res.render("logedinhome", { log: logedInUserData[0] });
+            res.render("templates/logedinhome", { log: logedInUserData[0] });
         else
-            res.render("login", { flag: 2 });
+            res.render("templates/login", { flag: 2 });
     } else {
-        res.render("login", { flag: 2 });
+        res.render("templates/login", { flag: 2 });
     }
 });
 
 app.get('/Createaccount', function(req, res) {
-    res.render('createaccount', { flag: 0 });
+    res.render('templates/createaccount', { flag: 0 });
 });
 
 app.post('/Createaccount', async function(req, res) {
@@ -68,34 +69,34 @@ app.post('/Createaccount', async function(req, res) {
         verificationNum = await DB.isFound(createAccountData.emailCA);
         console.log(verificationNum);
         if (verificationNum == 0) {
-            res.render("createaccount", { flag: 2 });
+            res.render("templates/createaccount", { flag: 2 });
         } else {
             const message = `<h4>Welcome to bookMyCelebration... <br> Here your verification code is </h4> <h1> ${verificationNum} </h1>`;
             await DB.sendEMail(createAccountData.emailCA, "Verify your self on bookMyCelebration...", message);
-            res.redirect("/OTPVer");
+            res.render("templates/OTPVer");
         }
     } else {
-        res.render("createaccount", { flag: 1 });
+        res.render("templates/createaccount", { flag: 1 });
     }
 });
 
 app.get('/OTPVer', function(req, res) {
-    res.render("OTPVer", { mail: createAccountData.emailCA });
+    res.render("templates/OTPVer", { mail: createAccountData.emailCA });
 });
 
 app.post('/OTPVer', function(req, res) {
     if (verificationNum == req.body.OTPVerification) {
         DB.CreateAccount(createAccountData);
-        res.render("login", { flag: 1 });
+        res.render("templates/login", { flag: 1 });
     }
 });
 
 app.get('/logedinhome', function(req, res) {
-    res.render('logedinhome', { log: logedInUserData[0] })
+    res.render('templates/logedinhome', { log: logedInUserData[0] })
 });
 
 app.get('/forgot', function(req, res) {
-    res.render('forgot');
+    res.render('templates/forgot');
 });
 
 app.post('/forgot', function(req, res) {
@@ -103,15 +104,19 @@ app.post('/forgot', function(req, res) {
 });
 
 app.get('/Profile', function(req, res) {
-    res.render('profile', { log: logedInUserData[0] });
+    res.render('templates/profile', { log: logedInUserData[0] });
 });
 
 app.get('/Gallery', function(req, res) {
-    res.render('Gallery', { log: logedInUserData[0] })
+    res.render('templates/Gallery', { log: logedInUserData[0] })
 });
 
 app.get('/about', function(req, res) {
-    res.render('aboutus');
+    res.render('templates/aboutusLog');
+});
+
+app.get('/aboutlog', function(req, res) {
+    res.render('templates/aboutusLogedIn', { log: logedInUserData[0] });
 });
 
 //Activate website on specific port
